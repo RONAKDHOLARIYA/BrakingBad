b513cf56e61ac8635504e3437c99fef54e40145f
Ronak Perception <ronakperception@192.168.1.10>
2022-03-16 17:20:46 +0530

    add network connection check method

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import React from 'react';
-import {StatusBar, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StatusBar,
+  SafeAreaView,
+  Modal,
+  ImageBackground,
+  Dimensions,
+  Text,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './App/Navigations/StackNavigation';
 import {colors} from './App/Helpers/colors';
-const App = ({params}) => (
-  <SafeAreaView style={{flex: 1, backgroundColor: colors.secondary}}>
-    <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
-    <NavigationContainer
-      theme={{
-        colors: {
-          card: colors.secondary,
-        },
-      }}>
-      <StackNavigation />
-    </NavigationContainer>
-  </SafeAreaView>
-);
+import {images} from './App/Helpers/images';
+import NetInfo from '@react-native-community/netinfo';
+const App = ({params}) => {
+  let [showNoNetwork, setShowNoNetwork] = useState(false);
+  useEffect(() => {
+    // check netwok connection
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setShowNoNetwork(!state.isConnected);
+    });
+    // Unsubscribe netwok method
+    return () => unsubscribe();
+  }, []);
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: colors.secondary}}>
+      <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
+      <NavigationContainer
+        theme={{
+          colors: {
+            card: colors.secondary,
+          },
+        }}>
+        <StackNavigation />
+      </NavigationContainer>
+      {showNoNetwork && (
+        <Modal visible={showNoNetwork} animationType="slide">
+          <ImageBackground
+            source={images.poster}
+            style={{
+              width: Dimensions.get('screen').width,
+              height: Dimensions.get('screen').height,
+            }}>
+            <Text
+              style={{
+                position: 'absolute',
+                bottom: 150,
+                color: colors.lightGreen,
+                textAlign: 'center',
+                width: '100%',
+                fontSize: 22,
+                fontWeight: '700',
+                lineHeight: 40,
+                backgroundColor: 'rgba(0,0,0,.3)',
+              }}>
+              Check Your Internet{`\n`}Connection.
+            </Text>
+          </ImageBackground>
+        </Modal>
+      )}
+    </SafeAreaView>
+  );
+};
 
 export default App;
--- App/Helpers/images.js
+++ App/Helpers/images.js
@@ -1,4 +1,5 @@
 export let images = {
   backIcon: require('../Assets/Images/back.png'),
   splashBanner: require('../Assets/Images/splashBanner.jpeg'),
+  poster: require('../Assets/Images/poster.jpg'),
 };
